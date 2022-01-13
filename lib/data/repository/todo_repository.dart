f018f06773db1b12daeac7c3a338134bf1b49277
Reuben <reuben.paterno@gmail.com>
2022-01-13 08:11:59 +0800

    add todo repository

--- lib/data/repository/todo_repository.dart
+++ lib/data/repository/todo_repository.dart
@@ -1,3 +1,4 @@
+import 'package:todo_app_redux/data/dao/task_dao.dart';
 import 'package:todo_app_redux/data/model/todo.dart';
 
 abstract class AbstractTodoRepository {
@@ -5,4 +6,19 @@ abstract class AbstractTodoRepository {
   Future<List<Todo>> read();
   Future<void> update(Todo todo);
   Future<void> delete(Todo todo);
+}
+
+class TodoRepository implements AbstractTodoRepository {
+  final AbstractTodoDao todoDao = TodoDao();
+  @override
+  Future<void> create(Todo todo) => todoDao.createTodo(todo);
+
+  @override
+  Future<List<Todo>> read() => todoDao.selectAllTodos();
+
+  @override
+  Future<void> update(Todo todo) => todoDao.updateTodo(todo);
+
+  @override
+  Future<void> delete(Todo todo) => todoDao.deleteTodo(todo);
 }
