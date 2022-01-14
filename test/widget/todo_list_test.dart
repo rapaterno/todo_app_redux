5ac805e0659e79b98c2cbe8ce72906370f9cef04
Reuben <reuben.paterno@gmail.com>
2022-01-14 15:01:14 +0800

    add store to tests

--- test/widget/todo_list_test.dart
+++ test/widget/todo_list_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_app_redux/data/model/todo.dart';
+import 'package:todo_app_redux/domain/app/app_state.dart';
+import 'package:todo_app_redux/domain/store.dart';
 import 'package:todo_app_redux/presentation/shared/todo_list/todo_list.dart';
 
 import '../mock_todos.dart';
@@ -8,11 +11,14 @@ import '../mock_todos.dart';
 void main() {
   group('TodoList', () {
     testWidgets('should show todo tiles', (tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          body: TodoList(mockTodos, (Todo todo) {}, (Todo todo) {}),
-        ),
-      ));
+      final store = createStore();
+      await tester.pumpWidget(StoreProvider<AppState>(
+          store: store,
+          child: MaterialApp(
+            home: Scaffold(
+              body: TodoList(mockTodos, (Todo todo) {}, (Todo todo) {}),
+            ),
+          )));
 
       await tester.pumpAndSettle();
 
--- test/widget/todo_tile_test.dart
+++ test/widget/todo_tile_test.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:todo_app_redux/data/model/todo.dart';
+import 'package:todo_app_redux/domain/app/app_state.dart';
+import 'package:todo_app_redux/domain/store.dart';
 import 'package:todo_app_redux/presentation/shared/todo_list/todo_tile.dart';
 
 import '../mock_todos.dart';
 
 void main() {
   testWidgets('Todo Tile', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Container(
-            child: TodoTile(mockTodos[0], (Todo todo) {}, (Todo todo) {})),
-      ),
-    ));
+    final store = createStore();
+    await tester.pumpWidget(StoreProvider<AppState>(
+        store: store,
+        child: MaterialApp(
+          home: Scaffold(
+            body: Container(
+                child: TodoTile(mockTodos[0], (Todo todo) {}, (Todo todo) {})),
+          ),
+        )));
 
     expect(find.text(mockTodos[0].name), findsOneWidget);
     expect(find.byIcon(Icons.check_box_outline_blank), findsOneWidget);
